a11dd68552a0945aa8b27c14e2f6b9a8601385ed
Luv Aggarwal <luvkaggarwal@gmail.com>
2016-12-22 20:48:50 +0530

    Init function defined

--- media.py
+++ media.py
@@ -1,1 +1,6 @@
 class Movie():
+    def __init__(self, movie_title, movie_storyline, poster_image, youtube_trailer_url):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.image = poster_image
+        self.trailer = youtube_trailer_url
