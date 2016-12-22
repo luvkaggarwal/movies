fab875cdc82a581bfeac6572a4f58ebd7e998fb6
Luv Aggarwal <luvkaggarwal@gmail.com>
2016-12-22 23:09:25 +0530

    Final Commit

--- media.py
+++ media.py
@@ -1,6 +1,12 @@
 import webbrowser
 
 class Movie():
+
+    #Adding documentation to any class
+    """This class stores the details of a movie and accesses its trailer"""
+    
+    VALID_RATINGS = ["G","PG","PG-13","R"]
+    
     def __init__(self, movie_title, movie_storyline, poster_image, youtube_trailer_url):
         self.title = movie_title
         self.storyline = movie_storyline
