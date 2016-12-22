b5165efe1f6753dd10ef45f6a3c95f2ced4a8798
Luv Aggarwal <luvkaggarwal@gmail.com>
2016-12-22 21:47:29 +0530

    Webpage Layout created

--- media.py
+++ media.py
@@ -1,6 +1,11 @@
+import webbrowser
+
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image, youtube_trailer_url):
         self.title = movie_title
         self.storyline = movie_storyline
         self.image = poster_image
         self.trailer = youtube_trailer_url
+
+    def showTrailer(self):
+        webbrowser.open(self.trailer)
